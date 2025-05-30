6bc9c3391c287e9c6edefdfec13842429d40115a
AHMAD IRYANA <ahmadiryana86@gmail.com>
2025-05-30 15:17:05 +0700

    fix video2

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,8 @@ export default defineConfig(({ mode }) => {
     return {
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+        'process.env.VITE_VIDEO': JSON.stringify(env.VITE_VIDEO)
       },
       resolve: {
         alias: {
